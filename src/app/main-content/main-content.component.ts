24116a903f866e4d83a2c61b7cfa88f5d21d83e3
MirkanPolat <Mirkanpolat@outlook.de>
2025-05-30 13:45:09 +0200

    adding about-me.component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeroSectionComponent],
+  imports: [HeroSectionComponent, AboutMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
