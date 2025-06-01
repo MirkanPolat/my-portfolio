0170cef15460e58f68a15293d38c4f999c790e26
MirkanPolat <Mirkanpolat@outlook.de>
2025-06-01 11:28:49 +0200

    adding skill-set.component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillSetComponent } from './skill-set/skill-set.component';
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeroSectionComponent, AboutMeComponent],
+  imports: [HeroSectionComponent, AboutMeComponent, SkillSetComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
