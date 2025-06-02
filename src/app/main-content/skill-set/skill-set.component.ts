a1b4374e8253b5d642530cd7cad8f488d3a49e16
MirkanPolat <Mirkanpolat@outlook.de>
2025-06-02 16:32:37 +0200

    Add new skill icons for various technologies. These icons are part of the design material for the project and enhance the visual representation of the skills used.

--- src/app/main-content/skill-set/skill-set.component.ts
+++ src/app/main-content/skill-set/skill-set.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Skill {
+  name: string;
+  icon: string;
+  row: number;
+}
 
 @Component({
   selector: 'app-skill-set',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skill-set.component.html',
   styleUrl: './skill-set.component.scss'
 })
 export class SkillSetComponent {
+  skills: Skill[] = [
+    { name: 'HTML', icon: 'assets/Design-material/img/Skill-Icons/HTML.png', row: 1 },
+    { name: 'CSS', icon: 'assets/Design-material/img/Skill-Icons/CSS.png', row: 1 },
+    { name: 'JavaScript', icon: 'assets/Design-material/img/Skill-Icons/Js.png', row: 1 },
+    { name: 'TypeScript', icon: 'assets/Design-material/img/Skill-Icons/Ts.png', row: 1 },
+    { name: 'Angular', icon: 'assets/Design-material/img/Skill-Icons/Angular.png', row: 1 },
+    { name: 'Firebase', icon: 'assets/Design-material/img/Skill-Icons/Firebase.png', row: 2 },
+    { name: 'Git', icon: 'assets/Design-material/img/Skill-Icons/Git.png', row: 2 },
+    { name: 'Rest-Api', icon: 'assets/Design-material/img/Skill-Icons/Rest-Api.png', row: 2 },
+    { name: 'Scrum', icon: 'assets/Design-material/img/Skill-Icons/Scrum.png', row: 2 },
+    { name: 'Material Design', icon: 'assets/Design-material/img/Skill-Icons/Material Design.png', row: 2 }
+  ];
 
+  getSkillsInRow(rowNumber: number): Skill[] {
+    return this.skills.filter(skill => skill.row === rowNumber);
+  }
 }
