2ba8f9336fefc3a727bace5c69bfa81dd6e2b87d
MirkanPolat <Mirkanpolat@outlook.de>
2025-06-03 17:49:06 +0200

    Add MyProjects component and update main content structure; adjust skill set animations for improved performance

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillSetComponent } from './skill-set/skill-set.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeroSectionComponent, AboutMeComponent, SkillSetComponent],
+  imports: [HeroSectionComponent, AboutMeComponent, SkillSetComponent, MyProjectsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
