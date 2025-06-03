0144ca48713dfc1a1e90831d71746b2c5e94e46b
MirkanPolat <Mirkanpolat@outlook.de>
2025-06-03 21:12:58 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/main-content/my-projects/my-projects.component.ts
+++ src/app/main-content/my-projects/my-projects.component.ts
@@ -5,6 +5,7 @@ interface Project {
   id: string;
   title: string;
   description: string;
+  image: string; // Neues Property für Projekt-Bild
   featured?: boolean;
   badgeImage?: string;
 }
@@ -30,17 +31,20 @@ export class MyProjectsComponent {
       title: 'Join',
       description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
       featured: true,
-      badgeImage: 'assets/Design-material/03_Stickers/project_imgs/featured.svg'
+      badgeImage: 'assets/Design-material/03_Stickers/project_imgs/featured.svg',
+      image: 'assets/Design-material/03_Stickers/project_imgs/Laptop.png'
     },
     {
       id: 'el-pollo-loco',
       title: 'El Pollo Loco',
-      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.'
+      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      image: 'assets/Design-material/03_Stickers/project_imgs/El-Pollo-Loco.png' 
     },
     {
       id: 'dabubble',
       title: 'DABubble',
-      description: 'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.'
+      description: 'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.',
+      image: 'assets/Design-material/03_Stickers/project_imgs/DABubble.png'
     }
   ];
 }
