e5db9638e5f596ac876ba4465923084e9a93d451
MirkanPolat <Mirkanpolat@outlook.de>
2025-06-08 17:19:13 +0200

    Add overlay component and update routing; enhance project details button with hover effects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
+import { OverlayComponent } from './overlay/overlay.component';
 
 export const routes: Routes = [
-    {path: '', component: MainContentComponent }
+    {path: '', component: MainContentComponent },
+    {path: '', component: OverlayComponent }
+   
+
+
 ];
