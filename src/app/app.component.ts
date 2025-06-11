11d5fc4a61115b126f41bff2788a76fbb0529331
MirkanPolat <Mirkanpolat@outlook.de>
2025-06-11 20:09:58 +0200

    Add ngx-translate for internationalization support; configure default language and translation services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,28 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { filter } from 'rxjs/operators';
+import {
+    TranslateService,
+    TranslatePipe,
+    TranslateDirective
+} from "@ngx-translate/core";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule, TranslatePipe, TranslateDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+
+   constructor(private translate: TranslateService) {
+        this.translate.addLangs(['de', 'en']);
+        this.translate.setDefaultLang('en');
+        this.translate.use('en');
+    }
+
   title = 'Mirkan-Polat-Portfolio';
   isOverlayRoute = false;
   
