1164e7a05ea230d1e2d8981dc99aef73cb2c30ae
MirkanPolat <Mirkanpolat@outlook.de>
2025-08-20 18:44:29 +0200

    Update email recipient in sendMail.php to use the correct contact address

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'Mirkanpolat@outlook.de';  
+            $recipient = 'kontakt@mirkanpolat.com';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
