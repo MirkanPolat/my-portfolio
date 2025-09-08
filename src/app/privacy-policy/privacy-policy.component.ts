16dc75147c4ea973e93e1959f1fb369e9ffcc073
MirkanPolat <Mirkanpolat@outlook.de>
2025-09-08 08:27:18 +0200

    Refactor Privacy Policy component: replace translation pipes with static text and simplify imports

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [CommonModule, TranslatePipe],
+  imports: [CommonModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
